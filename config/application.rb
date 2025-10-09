c69f09053758e33f8a871dae766de11eea2f7561
sekito <sekito1107gg@gmail.com>
2025-10-09 15:02:36 +0900

    新しく生成されるviewがslimになるように変更

--- config/application.rb
+++ config/application.rb
@@ -18,6 +18,10 @@ module Php8Study
     # Common ones are `templates`, `generators`, or `middleware`, for example.
     config.autoload_lib(ignore: %w[assets tasks])
 
+    config.generators do |g|
+      g.template_engine :slim
+    end
+
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
